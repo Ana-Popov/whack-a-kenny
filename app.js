fec3a4f4cacbdb3175fe7bf65113e4f6c2d69d44
Ana Popov <ana@Anas-MacBook-Pro.local>
2018-10-25 11:35:25 +0200

    tested

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ var app = new Vue({
             app.guess();
             this.seconds = app.timer(20);
             setTimeout(() => this.ended = true, 20000)
+
+        },
+
+        checkClicks: function() {
+            if (this.inGame == true) {
+                this.click++
+            }
         },
 
 
@@ -31,7 +38,6 @@ var app = new Vue({
             const idBox = Math.floor(Math.random() * boxes.length);
             const selectedBox = boxes[idBox];
             if (selectedBox === this.lastBox) {
-                console.log("duplicate box");
                 return app.getSouthPark(boxes)
             }
             this.lastBox = selectedBox;
@@ -41,15 +47,16 @@ var app = new Vue({
         highestScore: function () {
             localStorage["last-score"] = this.score;
             let highscore = localStorage["highest-score"];
-            //            this.highestScore = 0;9
             localStorage["last-score"] = app.score;
             if (localStorage["last-score"] > localStorage["highest-score"] || localStorage["highest-score"] == null) {
                 localStorage["highest-score"] = localStorage["last-score"];
             }
         },
 
+
+
         guess: function () {
-            const time = app.getTime(900, 1800);
+            const time = app.getTime(700, 1700);
             let boxes = document.querySelectorAll('.hole');
             const box = app.getSouthPark(boxes);
             box.classList.add('kenny');
@@ -64,16 +71,15 @@ var app = new Vue({
         },
 
         hitKenny: function (e) {
-            console.log(e);
             let el = document.getElementsByClassName('hole');
             if (e.isTrusted) {
                 this.score++;
                 e.toElement.parentNode.classList.remove('kenny');
-
             } else {
                 alert("Hey cheater!");
             }
         },
+
         timer: function (seconds) {
             const counting = new Date().getTime() + 20000;
             let x = setInterval(() => {
@@ -81,7 +87,7 @@ var app = new Vue({
                 let distance = counting - now;
                 let seconds = Math.floor((distance % (1000 * 60)) / 1000)
                 app.seconds = 'Time left: ' + seconds + ' seconds left';
-                if (distance <= 5) {
+                if (distance < 0) {
                     clearInterval(x);
                     app.seconds = "GAME OVER";
                     app.highestScore();
